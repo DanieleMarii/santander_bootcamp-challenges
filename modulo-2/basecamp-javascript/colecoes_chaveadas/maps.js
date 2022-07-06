1600c340f06164c872ba3e293cca10704211f099
DanieleMarii <mdaniele2411@gmail.com>
2022-07-05 21:20:16 -0300

    resolvendo o primeiro desafio sobre maps

---
+++ modulo-2/basecamp-javascript/colecoes_chaveadas/maps.js
@@ -0,0 +1,17 @@
+function getAdmins(map) {
+    let admins = [];
+    for ([key, value] of map) {
+        if (value === 'Admin') {
+            admins.push(key);
+        }
+    }
+    return admins;
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luiz', 'Admin');
+usuarios.set('Mariane', 'Admin');
+usuarios.set("Beatriz", "User");
+
+console.log(getAdmins(usuarios));
